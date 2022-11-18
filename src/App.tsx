fc90283720aad0eccff2ac7e009bee067e991cce
LeeTaeGwan <taegwan.lee@ssvt.co.kr>
2022-11-18 16:48:05 +0900

    react-responsive default code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useMediaQuery } from 'react-responsive'
 
 function App() {
+
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1280px)'
+  })
+  const tabLg = useMediaQuery({
+    query: '(min-width: 960px) and (max-width: 1279px)'
+  })
+  const tabSm = useMediaQuery({
+    query: '(min-width: 600px) and (max-width: 959px)'
+  })
+  const mobile = useMediaQuery({
+    query: '(min-width: 0px) and (max-width: 599px)'
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isDesktop && <div style={{backgroundColor:'red'}}>Desktop</div>}
+      {tabLg && <div style={{backgroundColor:'orange'}}>TabLg</div>}
+      {tabSm && <div style={{backgroundColor:'yellow'}}>TabSm</div>}
+      {mobile && <div style={{backgroundColor:'green'}}>Mobile</div>}
     </div>
   );
 }
